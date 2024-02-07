f728f9308bd72f0f51d0399a35844c28fd70f4a0
Stella Bloom <windowz414@gnuweeb.org>
2024-02-07 14:48:08 +0100

    extra: version: Force full PixelBuilds version to be set as ours
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- version.mk
+++ version.mk
@@ -2,4 +2,4 @@
 PB_VERSION := PasuPare
 PB_BUILD_TYPE := PasuFic
 
-PIXELBUILDS_VERSION ?= Kasumi_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M))
+PIXELBUILDS_VERSION := Kasumi_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M))
