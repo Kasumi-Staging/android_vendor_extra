505d194a29daa919de76100d86afb3ce123f1816
Stella Bloom <windowz414@gnuweeb.org>
2024-02-08 04:19:55 +0100

    extra: version: Import versioning props
    * To work around our versioning being overridden by PB ones during
      build prop generation.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- version.mk
+++ version.mk
@@ -3,3 +3,12 @@ PB_VERSION := PasuPare
 PB_BUILD_TYPE := PasuFic
 
 PIXELBUILDS_VERSION := Kasumi_$(TARGET_PRODUCT_SHORT)-$(PB_VERSION)-$(shell date +%Y%m%d-%H%M)
+
+# There must be a better way to have build system handle these props,
+# copying the configuration from base versioning Makefile feels so
+# wrong.
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.version.custom=$(PB_VERSION) \
+    ro.build.version.device=$(TARGET_PRODUCT_SHORT) \
+    ro.pb.build.version=$(PIXELBUILDS_VERSION) \
+    ro.pb.buildtype=$(PB_BUILD_TYPE)
